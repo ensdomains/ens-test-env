54c48e2cc409da8940b9984b24ffe6dc9d30f617
v1rtl <hi@v1rtl.site>
2025-05-08 11:19:56 +0300

    fix hardhat setup

--- ens-test-env.config.js
+++ ens-test-env.config.js
@@ -17,28 +17,30 @@ process.env.BATCH_GATEWAY_URLS = JSON.stringify([
 ])
 
 /**
- * @type {import('@ensdomains/ens-test-env').ENSTestEnvConfig}
+ * @type {import('./src/index').ENSTestEnvConfig}
  **/
-export const deployCommand = 'pnpm hh --network localhost deploy --no-compile'
-export const buildCommand = 'pnpm build:glocal && pnpm export'
-export const scripts = [
-  {
-    command: 'pnpm wrangle',
-    name: 'wrangler',
-    prefixColor: 'magenta.bold',
+export default {
+  deployCommand: 'pnpm hh --network localhost deploy --no-compile',
+  buildCommand: 'pnpm build:glocal && pnpm export',
+  scripts: [
+    {
+      command: 'pnpm wrangle',
+      name: 'wrangler',
+      prefixColor: 'magenta.bold',
+    },
+    {
+      command: `pnpm wait-on http://localhost:8788 && ${
+        process.env.CI
+          ? `pnpm playwright test --project=stateless --shard=${process.env.PLAYWRIGHT_SHARD}/${process.env.PLAYWRIGHT_TOTAL}`
+          : 'pnpm playwright'
+      }`,
+      name: 'playwright',
+      prefixColor: 'yellow.bold',
+      env: process.env,
+      finishOnExit: true,
+    },
+  ],
+  paths: {
+    data: './data',
   },
-  {
-    command: `pnpm wait-on http://localhost:8788 && ${
-      process.env.CI
-        ? `pnpm playwright test --project=stateless --shard=${process.env.PLAYWRIGHT_SHARD}/${process.env.PLAYWRIGHT_TOTAL}`
-        : 'pnpm playwright'
-    }`,
-    name: 'playwright',
-    prefixColor: 'yellow.bold',
-    env: process.env,
-    finishOnExit: true,
-  },
-]
-export const paths = {
-  data: './data',
 }
