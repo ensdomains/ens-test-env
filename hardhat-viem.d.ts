92688beb1d94a41c41b63f0552fbbc7f24db6d4f
v1rtl <hi@v1rtl.site>
2025-05-09 10:37:40 +0300

    fix types

--- hardhat-viem.d.ts
+++ hardhat-viem.d.ts
@@ -1,5 +1,7 @@
+import type { config } from './hardhat.config.js'
+
 declare module '@nomicfoundation/hardhat-viem/types.js' {
   interface Register {
-    config: typeof config
+    config: config
   }
 }
